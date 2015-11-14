36c504f3e4f31f4945178b2f42a7c9fadebd3df3
Casey Yang <knightykc@gmail.com>
2015-11-15 01:37:14 +0800

    add main.cpp in SemanticRoadNetworkGenerator;

---
+++ SemanticRoadNetworkGenerator/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iterator>
+#include "../MapLibraries/Map.h"
+using namespace std;
+
+string filePath = "E:\\MapMatchingProject\\Data\\新加坡数据\\";
+string inputDirectory = "9daysForTrajPattern\\input";
+string answerDirectory = "9daysForTrajPattern\\answer";
+Map routeNetwork(filePath, 500);
+
+void main() {
+
+}
