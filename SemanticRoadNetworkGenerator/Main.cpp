11b3e88ff98a7ad273633493d136f560cb074003
Casey Yang <knightykc@gmail.com>
2015-11-15 11:43:16 +0800

    update;

--- SemanticRoadNetworkGenerator/Main.cpp
+++ SemanticRoadNetworkGenerator/Main.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 string filePath = "E:\\MapMatchingProject\\Data\\新加坡数据\\";
+string POIfilePath = "poi.txt";
 string inputDirectory = "9daysForTrajPattern\\input";
 string answerDirectory = "9daysForTrajPattern\\answer";
 Map routeNetwork(filePath, 500);
 
 void main() {
-
+	ifstream fin(POIfilePath);
 }
