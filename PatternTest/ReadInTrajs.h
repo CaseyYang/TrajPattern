34014e70bf5e97d7691d1769021d0cc9d3fb99d9
Casey Yang <knightykc@gmail.com>
2015-12-21 15:16:07 +0800

    add function readMergedMapMatchingResult;

--- PatternTest/ReadInTrajs.h
+++ PatternTest/ReadInTrajs.h
@@ -6,16 +6,21 @@
 #include "../MapLibraries/Map.h"
 
 //读入给定路径的轨迹文件中的一条轨迹，把每个轨迹采样点插入对应的时间片中
-extern void readOneTrajectory(string &filePath);
+extern void readOneTrajectory(string &filePath, int trajId, vector<TimeSlice*> &timeSlices);
 //读入给定路径的地图匹配结果文件，把每个匹配路段插入对应的时间片中
-void readOneMapMatchingResult(string &filePath, int trajId, vector<NewTimeSlice*> &timeSlices, Map &routeNetwork);
+extern void readOneMapMatchingResult(string &filePath, int trajId, vector<NewTimeSlice*> &timeSlices, Map &routeNetwork);
+//读入给定路径的地图匹配结果，把每个匹配路段插入对应的时间片中（对应读结果合并文件版本）
+extern int readOneMapMatchingResultV2(ifstream &fin, int trajId, vector<NewTimeSlice*> &timeSlices, Map &routeNetwork);
 //读入文件夹中所有轨迹文件，保存轨迹和时间片
 extern void scanTrajFolder(string folderDir, string inputDirectory, vector<TimeSlice*> &timeSlices);
 //读入地图匹配结果文件，保存时间片
 extern void scanMapMatchingResultFolder(string folderDir, string inputDirectory, vector<NewTimeSlice*> &timeSlices, Map &routeNetwork);
+//读入合并的地图匹配文件，保存时间片
+extern void readMergedMapMatchingResult(string filePath, vector<NewTimeSlice*> &timeSlices, Map &routeNetwork);
+
 //读入路段附近POI统计文件，填充每个路段的poiNums数组
 extern void generateSemanticRoad(Map&routeNetwork, string filePath);
 //输出路段id和poiNum数组到指定文件
-extern void outputSemanticRouteNetwork(Map&routeNetwork, string filePath);
+extern void outputSemanticRouteNetworkToPlainText(Map&routeNetwork, string filePath);
 //输出TimeSlice
 extern void outputTimeSlices(vector<TimeSlice*> &timeSlices);
